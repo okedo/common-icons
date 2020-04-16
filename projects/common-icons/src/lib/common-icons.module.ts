684f0829c06f36d01a661836d5c0a1983cf32354
Viktor Patraboi <viktor.patraboi@medhost.com>
2020-04-16 16:03:27 +0300

    added common module

--- projects/common-icons/src/lib/common-icons.module.ts
+++ projects/common-icons/src/lib/common-icons.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonIconComponent } from './common-icon.component';
+import { CommonModule } from '@angular/common';
 
+import { CommonIconComponent } from './common-icon.component';
 
 
 @NgModule({
   declarations: [CommonIconComponent],
+  imports: [CommonModule],
   exports: [CommonIconComponent]
 })
 export class CommonIconsModule { }
